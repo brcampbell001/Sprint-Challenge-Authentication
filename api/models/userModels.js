9f38df13082fcbb1afe07aee79bde4c1be131890
unknown <brcampbell001@outlook.com>
2018-03-02 12:57:03 -0600

    User Schema Added

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -1,8 +1,18 @@
+// Thank you for reviewing my code! :)
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = Schema({
-  // create your user schema here.
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model('User', UserSchema);
